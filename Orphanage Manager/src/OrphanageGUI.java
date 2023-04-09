8f7b451b2fe06db21b8d48a3998c028a329bd92a
DariusX1 <116304136+DariusX1@users.noreply.github.com>
2023-04-09 16:31:54 -0400

    Changes to GUI

--- Orphanage Manager/src/OrphanageGUI.java
+++ Orphanage Manager/src/OrphanageGUI.java
@@ -1,6 +1,9 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -11,19 +14,31 @@ public class OrphanageGUI extends Application {
     }
 
     public void start(final Stage mainStage) {
-        Pane root = new Pane();
+        BorderPane root = new BorderPane();
         setUpControls(root);
-        Scene scene = new Scene(root, 600, 800);
+        Scene scene = new Scene(root, 500, 500);
         setStage(mainStage, scene);
     }
 
-    private void setUpControls(Pane mainPane) {
+    private void setUpControls(BorderPane mainPane) {
+     HBox top = new HBox(100); 
+     HBox bottom = new HBox(100); 
+     
+     Label label = new Label("This is the top HBox"); 
+     Label label2 = new Label("This is the botton HBox");
+
+     top.getChildren().add(label); 
+     bottom.getChildren().add(label2);
+
+     mainPane.setTop(top);
+     mainPane.setBottom(bottom);
      
     }
 
     private void setStage(Stage mainStage, Scene scene) {
+        mainStage.setTitle("Orphange");
         mainStage.setScene(scene);
-         mainStage.show();
+        mainStage.show();
         
     }
 }

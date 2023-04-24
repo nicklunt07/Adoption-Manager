0350346e5f636f341884111299e70e1c4face239
DariusX1 <116304136+DariusX1@users.noreply.github.com>
2023-04-23 23:30:42 -0400

    changes

--- Orphanage Manager/src/Taskable.java
+++ Orphanage Manager/src/Taskable.java
@@ -2,11 +2,10 @@ import java.io.Serializable;
 
 import javafx.scene.control.Button;
 
-public interface  Taskable extends Serializable{
+public interface Taskable extends Serializable{
     
     public Skillable getSkill(); 
     public void assignTask(Skillable skill); 
     public Button performTask();
    
-
 }
